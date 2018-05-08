8abd1b624d4d2c6a1130384ce3a1110f82886f91
Kushal Jain <kushal.jain@codalien.com>
2018-05-08 19:56:25 +0530

    new module for calender

--- app/screen/Home/index.js
+++ app/screen/Home/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Container, Header, Title, Content, Button, Right, Body} from 'native-base';
-import {Modal, Dimensions, View, Text, TextInput} from 'react-native';
-import DatepickerRange from 'react-native-range-datepicker';
+import {Modal, View, TextInput, Text, TouchableOpacity} from 'react-native';
+import {Calendar} from 'react-native-calendars';
+import {Feather, FontAwesome} from '@expo/vector-icons';
 import moment from 'moment';
 import Styles from './styles';
 
-const window = Dimensions.get('window');
+const colorsData = {
+    orange: '#FF7F50',
+    white: '#ffffff'
+};
+
+const formatString = 'YYYY-MM-DD';
 
 class homeScreen extends React.Component {
     static navigationOptions = {
@@ -17,8 +23,26 @@ class homeScreen extends React.Component {
         super();
         this.state = {
             modalVisible: false,
-            startDate: '',
-            untilDate: ''
+            startDate: null,
+            endDate: null,
+            startDateTemp: moment().endOf('day'),
+            endDateTemp: moment().add(1, 'days').endOf('day'),
+            selectorStart: false,
+            markedDates: {
+                [moment().format(formatString)]: {
+                    color: colorsData.orange,
+                    textColor: colorsData.white,
+                    startingDay: true,
+                    endingDay: true
+                },
+                [moment().add(1, 'days').format(formatString)]: {
+                    color: colorsData.orange,
+                    textColor: colorsData.white,
+                    startingDay: true,
+                    endingDay: true
+                }
+            },
+            initialStatus: true
         }
     }
 
@@ -28,33 +52,119 @@ class homeScreen extends React.Component {
         })
     }
 
-    closeModal() {
-        this.toggleCalender();
+    changeDateState(status) {
+        console.log('changeDateState', status)
+        this.setState({
+            selectorStart: status
+        })
     }
 
-    setDates() {
-        this.toggleCalender();
-    }
-
-    onSelectDate(startDate, untilDate) {
-        let years = moment(startDate).diff(untilDate, 'years');
-
-        //Check if Data Booking Time Date is Less Than a Year and More Than 3 Days
-        if (!Number.isNaN(years) && years === 0) {
-            this.setState({startDate, untilDate});
+    dayPressed(day) {
+        console.log('pressed called');
+        const dateObj = moment(day['timestamp']).endOf('day');
+        let newState = {};
+        console.log(dateObj, (this.state.startDateTemp.diff(dateObj, 'days')), (this.state.endDateTemp.diff(dateObj, 'days')));
+        if (this.state.startDateTemp && this.state.endDateTemp) {
+            if ((this.state.startDateTemp.diff(dateObj, 'days') < 0) && (this.state.endDateTemp.diff(dateObj, 'days') > 0)) {
+                console.log('between');
+                if (this.state.selectorStart) {
+                    newState = {
+                        startDateTemp: dateObj,
+                        endDateTemp: this.state.endDateTemp,
+                        selectorStart: false
+                    }
+                } else {
+                    newState = {
+                        startDateTemp: this.state.startDateTemp,
+                        endDateTemp: dateObj,
+                        selectorStart: false
+                    }
+                }
+            } else if (this.state.startDateTemp.diff(dateObj, 'days') > 0) {
+                console.log('less');
+                if (this.state.selectorStart) {
+                    newState = {
+                        startDateTemp: dateObj,
+                        endDateTemp: this.state.endDateTemp,
+                        selectorStart: false
+                    }
+                } else {
+                    newState = {
+                        startDateTemp: dateObj,
+                        endDateTemp: dateObj.add(1, 'days'),
+                        selectorStart: false
+                    }
+                }
+            } else if (this.state.endDateTemp.diff(dateObj, 'days') < 0) {
+                console.log('greater');
+                console.log(this.state.selectorStart);
+                if (this.state.selectorStart) {
+                    newState = {
+                        startDateTemp: dateObj,
+                        endDateTemp: dateObj.add(1, 'days'),
+                        selectorStart: false
+                    }
+                } else {
+                    newState = {
+                        startDateTemp: this.state.startDateTemp,
+                        endDateTemp: dateObj,
+                        selectorStart: false
+                    }
+                }
+            }
+        } else if (this.state.startDateTemp || this.state.endDateTemp) {
+            console.log('never coming case')
+        } else {
+            console.log('no date selected');
+            newState = {
+                startDateTemp: dateObj,
+                endDateTemp: dateObj.add(1, 'days'),
+                selectorStart: false
+            }
         }
+        const markedDates = this.getMarkedDates(moment(newState.startDateTemp), moment(newState.endDateTemp));
+        newState.markedDates = markedDates;
+        this.setState(newState);
     }
 
-    componentWillReceiveProps(nextProps) {
+    getMarkedDates(start, end) {
+        console.log(start, end);
+        const result = {
+            [start.format(formatString)]: {
+                color: colorsData.orange,
+                textColor: colorsData.white,
+                startingDay: true,
+                endingDay: true
+            }
+        };
+        let temp = start.add(1, 'days');
+        while(temp.format(formatString) !== end.format(formatString)) {
+            result[temp.format(formatString)] = {
+                color: colorsData.white,
+                textColor: colorsData.orange
+            };
+            temp = temp.add(1, 'days');
+        }
+        result[end.format(formatString)] = {
+            color: colorsData.orange,
+            textColor: colorsData.white,
+            startingDay: true,
+            endingDay: true
+        };
+        console.log(result)
+        return result;
+    }
+
+    confirmDateSelection() {
+        this.setState({
+            initialStatus: false,
+            startDate: this.state.startDateTemp,
+            endDate: this.state.endDateTemp,
+            modalVisible: false
+        });
     }
 
     render() {
-        const currentDate = moment().format();
-        const format = 'hh:mm:ss';
-        const currentTime = moment()
-        const beforeTime = moment('00:00:00', format);
-        const afterTime = moment('04:00:00', format);
-
         return (
             <Container>
                 <Header>
@@ -82,30 +192,91 @@ class homeScreen extends React.Component {
                                hardwareAccelerated={true}
                                visible={this.state.modalVisible}
                                onRequestClose={this.toggleCalender.bind(this)}>
-                            <View style={{
-                                flex: 1,
-                                flexDirection: 'column',
-                                justifyContent: 'center',
-                                alignItems: 'center'
-                            }}>
+                            <View style={Styles.modalBackDrop}></View>
+                            <View style={Styles.modalParent}>
                                 <View style={Styles.modalView}>
-                                    <DatepickerRange
-                                        startDate={this.state.startDate ? moment(this.state.startDate).format() : ''}
-                                        untilDate={this.state.untilDate ? moment(this.state.untilDate).format() : ''}
-                                        onClose={this.closeModal.bind(this)}
-                                        onConfirm={this.setDates.bind(this)}
-                                        onSelect={this.onSelectDate.bind(this)}
-                                        buttonColor={'rgb(29,37,72)'}
-                                        todayColor={'#1D2548'}
-                                        minDate={currentTime.isBetween(beforeTime, afterTime) ? moment().subtract(1, 'days').format() : currentDate}
-                                        infoContainerStyle={{
-                                            marginRight: 20,
-                                            paddingHorizontal: 20,
-                                            paddingVertical: 5,
-                                            backgroundColor: '#1D2548',
-                                            borderRadius: 20,
-                                            alignSelf: 'center'
-                                        }}/>
+                                    <View style={Styles.calenderHeader}>
+                                        <TouchableOpacity activeOpacity={1}
+                                                          onPress={this.changeDateState.bind(this, true)}>
+                                            <View style={Styles.headerBlock}>
+                                                <View style={{flex: 1}}>
+                                                    <Text style={[Styles.textCheck, {
+                                                        textAlign: 'left',
+                                                        paddingLeft: 10
+                                                    }]}>Check In</Text>
+                                                </View>
+                                                <View style={{flex: 1}}>
+                                                    <Text style={[Styles.textDate, {
+                                                        textAlign: 'left',
+                                                        paddingLeft: 10
+                                                    }]}>{
+                                                        this.state.startDateTemp.format('ddd, D MMM')
+                                                    }</Text>
+                                                </View>
+                                                {
+                                                    this.state.selectorStart ?
+                                                        <View style={{flex: 1, paddingBottom: 0, marginBottom: 0}}>
+                                                            <FontAwesome name="caret-up" style={Styles.caretIcon}/>
+                                                        </View> : <View/>
+                                                }
+                                            </View>
+                                        </TouchableOpacity>
+                                        <View style={Styles.headerBlock}>
+                                            <Feather name="arrow-right" style={Styles.textIcon}/>
+                                        </View>
+                                        <TouchableOpacity activeOpacity={1}
+                                                          onPress={this.changeDateState.bind(this, false)}>
+                                            <View style={Styles.headerBlock}>
+                                                <View style={{flex: 1}}>
+                                                    <Text style={[Styles.textCheck, {
+                                                        textAlign: 'right',
+                                                        paddingRight: 10
+                                                    }]}>Check Out</Text>
+                                                </View>
+                                                <View style={{flex: 1}}>
+                                                    <Text style={[Styles.textDate, {
+                                                        textAlign: 'right',
+                                                        paddingRight: 10
+                                                    }]}>{
+                                                        this.state.endDateTemp.format('ddd, D MMM')
+                                                    }</Text>
+                                                </View>
+                                                {
+                                                    !this.state.selectorStart ?
+                                                        <View style={{flex: 1, paddingBottom: 0, marginBottom: 0}}>
+                                                            <FontAwesome name="caret-up" style={Styles.caretIcon}/>
+                                                        </View> : <View/>
+                                                }
+                                            </View>
+                                        </TouchableOpacity>
+                                    </View>
+                                    <Calendar
+                                        current={new Date(moment())}
+                                        minDate={new Date(moment())}
+                                        maxDate={new Date(moment().add(1, 'year'))}
+                                        onDayPress={this.dayPressed.bind(this)}
+                                        monthFormat={'MMM yyyy'}
+                                        markingType={'period'}
+                                        markedDates={this.state.markedDates}
+                                        theme={{
+                                            arrowColor: 'rgb(29,37,72)',
+                                            textDayFontSize: 12,
+                                            textMonthFontSize: 12,
+                                            textMonthFontWeight: 'bold',
+                                            textDayHeaderFontSize: 12,
+                                            selectedDayBackgroundColor: '#FF7F50',
+                                            selectedDayTextColor: '#ffffff',
+                                        }}
+                                    />
+                                    <View style={Styles.calenderFooter}>
+                                        <View style={Styles.footerBlock}>
+                                            <Text style={Styles.buttonCancel} onPress={this.toggleCalender.bind(this)}>Cancel</Text>
+                                        </View>
+                                        <View style={Styles.footerBlock}>
+                                            <Text style={Styles.buttonDone}
+                                                  onPress={this.confirmDateSelection.bind(this)}>Done</Text>
+                                        </View>
+                                    </View>
                                 </View>
                             </View>
                         </Modal>
--- app/screen/Home/styles.js
+++ app/screen/Home/styles.js
@@ -1,4 +1,6 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+
+const window = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     content: {
@@ -17,14 +19,85 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         flex: 1,
         padding: 10,
-        paddingTop:5  
+        paddingTop: 5
+    },
+    modalParent: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modalBackDrop: {
+        height: window.height,
+        width: window.width,
+        backgroundColor: '#000000',
+        opacity: 0.7,
+        position: 'absolute',
+        top: 0,
+        left: 0
     },
     modalView: {
-        height: 450,
-        width: 300,
-        alignSelf: 'center'
+        height: window.height * 0.7,
+        width: window.width * 0.8,
+        alignSelf: 'center',
+        backgroundColor: '#ffffff',
+        zIndex: 1000,
+        opacity: 1
+    },
+    calenderHeader: {
+        height: window.height * 0.7 * 0.15,
+        width: window.width * 0.8,
+        backgroundColor: 'rgb(29,37,72)',
+        flexDirection: 'row'
+    },
+    headerBlock: {
+        flex: 1,
+        flexDirection: 'column',
+        paddingTop: 16,
+        paddingBottom: 18
+    },
+    textIcon: {
+        color: '#ffffff',
+        alignSelf: 'center',
+        fontSize: 34
+    },
+    caretIcon: {
+        color: '#ffffff',
+        alignSelf: 'center',
+        fontSize: 24,
+        marginBottom: 0,
+        marginTop: 5
+    },
+    textCheck: {
+        color: '#ffffff',
+        fontSize: 12
+    },
+    textDate: {
+        color: '#ffffff',
+        fontSize: 18
+    },
+    calenderFooter: {
+        height: window.height * 0.7 * 0.15,
+        width: window.width * 0.8,
+        backgroundColor: '#ffffff',
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    footerBlock: {
+        flex: 0.25,
+        alignItems: 'center',
+        paddingBottom: 10
+    },
+    buttonCancel: {
+        color: '#7C7C7C',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    buttonDone: {
+        color: 'rgb(29,37,72)',
+        fontSize: 20,
+        fontWeight: 'bold'
     }
-
 });
 
 export default styles;
