0856dacaf47e81a5399c361fc8f2b03e0f31a87b
Kushal Jain <jain.kushal89@gmail.com>
2018-05-12 23:24:34 +0530

    reverted

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ import AppRoot from '@app/index.js';
 export default class App extends React.Component {
 	constructor (props) {
 		super(props);
-		console.log('App Init');
 		UIManager.setLayoutAnimationEnabledExperimental
 						&& UIManager.setLayoutAnimationEnabledExperimental(true);
 	}
